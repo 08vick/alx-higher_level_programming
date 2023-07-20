8e26d4ee974448c3e30a6319f4fc54ba62e1f96c
unknown <vickodhis.vn@gmail.com>
2023-07-20 13:01:29 +0300

    sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- insett these values into the tableid = 89, name = Holberton School
-INSERT INTO `first_table` (`id`, `name`) VALUES ("89", "Holberton School");
+-- Inserts a new row into the table first_table.
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
